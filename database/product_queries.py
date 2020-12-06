3bd76e08b6fd04e15bed1f44cd8752b8e45c170a
shahrukh <shoaibtayyab121@gmail.com>
2020-12-06 13:08:39 +0530

    Added queries for adding category and product via admin panel

--- database/product_queries.py
+++ database/product_queries.py
@@ -25,5 +25,20 @@ def get_product_detail_query(product_id):
     return product_detail_query
 
 
+def add_category_query(category_name, category_description):
+    add_category_query = f"""   INSERT INTO Categories (category_name, category_description)
+                                VALUES ('{category_name}', '{category_description}');
+                            """
+    print(add_category_query)
+    return add_category_query
+
+
+def add_product_query(product_name, product_price, category_id):
+    add_product_query = f"""INSERT INTO Product (product_name, product_price, category_id)
+                            VALUES ('{product_name}', '{product_price}', '{category_id}');
+                        """
+    return add_product_query
+
+
 def get_last_insert_id():
     return "SELECT LAST_INSERT_ID();"
