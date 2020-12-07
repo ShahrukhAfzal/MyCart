7661684543453d86555a02c7953f4baeb185e50d
shahrukh <shoaibtayyab121@gmail.com>
2020-12-07 23:07:38 +0530

    done code refactoring

--- task.py
+++ task.py
@@ -70,43 +70,41 @@ class User:
         mytable.title = 'List of all the Customer'
         print(mytable)
 
-    def get_all_bill_from_user(self):
-        self.get_all_user()
-        cursor = self.connection.cursor()
-        user_id = click.prompt(click.style('Enter the user_id to see its all bill', fg='yellow'), type=int)
-        cursor.execute(get_all_bill_by_user_query(user_id))
+    def get_all_bill_from_user(self, user_id=None):
+        if not user_id:
+            self.get_all_user()
+            user_id = click.prompt(click.style('Enter the user_id to see its all bill', fg='yellow'), type=int)
+
+        cursor = self.execute_query(get_all_bill_by_user_query(user_id))
         mytable = from_db_cursor(cursor)
         mytable.title = f"Bill of user {user_id}"
-
         print(mytable)
 
     def customer_flow(self):
-        choice_list = '\n1.Buy(Show category list)\t2.Show Cart\t3.exit\n'
-        choice = click.prompt(click.style('Please enter your choice (e.g. 1 or 2)', fg='yellow'), prompt_suffix=choice_list, type=int)
-
+        choice_list = ['Buy(Show category list)', 'Show Cart', 'exit']
+        choice = get_choice(choice_list)
         if choice == 1:
             self.list_all_categories()
-            choice_list = '\n1.Show Products\t 2.Main Menu\t 3.exit\n'
-            choice = click.prompt(click.style('Please enter your choice (e.g. 1 or 2)', fg='yellow'), prompt_suffix=choice_list, type=int)
+            choice_list = ['Show Products', 'Main Menu', 'exit']
+            choice = get_choice(choice_list)
 
             if choice == 1:
                 category_id = click.prompt(click.style('Enter category id to see list of products', fg='yellow'), type=int)
                 self.list_all_products(category_id)
-                choice_list = '\n1.Detail of product\t2.Main Menu\n'
-                choice = click.prompt(click.style('Please enter your choice (e.g. 1 or 2)', fg='blue'), prompt_suffix=choice_list, type=int)
+                choice_list = ['Detail of product', 'Main Menu', 'exit']
+                choice = get_choice(choice_list)
 
                 if choice == 1:
                     product_id = click.prompt(click.style('Enter product id to see its details', fg='bright_blue'), type=int)
                     self.detail_product(product_id)
-                    choice_list = '\n1.Do you want to add this item to the cart\t2.Main Menu\n'
-                    choice = click.prompt(click.style('Please enter your choice (e.g. 1 or 2)', fg='blue'), prompt_suffix=choice_list, type=int)
+                    choice_list = ['Do you want to add this item to the cart', 'Main Menu']
+                    choice = get_choice(choice_list)
 
                     if choice == 1:
                         self.add_to_cart(product_id)
-                        click.secho('Product successfully added to cart', fg='green')
                         self.view_cart()
-                        choice_list = '\n1.Buy\t2.Remove item from the cart\t3.main_menu\n'
-                        choice = click.prompt(click.style('Please enter your choice (e.g. 1 or 2)', fg='blue'), prompt_suffix=choice_list, type=int)
+                        choice_list = ['Buy', 'Remove item from the cart', 'Main Menu']
+                        choice = get_choice(choice_list)
 
                         if choice == 1:
                             self.buy_from_cart()
@@ -121,7 +119,7 @@ class User:
                 elif choice == 2:
                     return self.customer_flow()
                 else:
-                    return self.customer_flow()
+                    exit()
 
             elif choice == 2:
                 return self.customer_flow()
@@ -130,18 +128,17 @@ class User:
 
         elif choice == 2:
             self.view_cart()
-            choice_list = '\n1.Buy\t2.Remove item from the cart\t3.main_menu\n'
-            choice = click.prompt(click.style('Please enter your choice (e.g. 1 or 2)', fg='blue'), prompt_suffix=choice_list, type=int)
+            choice_list = ['Buy', 'Remove item from the cart', 'Main Menu']
+            choice = get_choice(choice_list)
 
             if choice == 1:
                 return self.buy_from_cart()
             elif choice == 2:
                 self.remove_from_cart()
                 self.view_cart()
-
                 return self.customer_flow()
             else:
-                self.customer_flow()
+                return self.customer_flow()
 
         else:
             exit()
@@ -180,8 +177,7 @@ class Product:
         print(mytable)
 
     def list_all_products(self, category_id):
-        cursor = self.connection.cursor()
-        cursor.execute(get_products_query(category_id))
+        cursor = self.execute_query(get_products_query(category_id))
         mytable = from_db_cursor(cursor)
         if cursor.rowcount:
             mytable.title = f'LIST OF ALL Product of category {category_id}'
@@ -192,8 +188,7 @@ class Product:
             return self.main()
 
     def detail_product(self, product_id):
-        cursor = self.connection.cursor()
-        cursor.execute(get_product_detail_query(product_id))
+        cursor = self.execute_query(get_product_detail_query(product_id))
         mytable = from_db_cursor(cursor)
         mytable.title = 'Product detail'
         print(mytable)
@@ -221,94 +216,101 @@ class Product:
         else:
             return self.admin_flow()
 
-    def add_product(self):
+    def add_product(self, product_name=None, product_price=None, category_id=None, ask_confirm=True, confirm=True):
         click.secho('Add product', fg='green')
-
-        product_name = click.prompt(click.style('Product name ?', fg='yellow'), type=str)
+        if not product_name:
+            product_name = click.prompt(click.style('Product name ?', fg='yellow'), type=str)
         if (len(product_name.strip()) < 3):
             click.secho('Error: Product name should be greater than 3 characters', fg='red')
             return self.add_product()
 
-        product_price = click.prompt(click.style('Price ?', fg='yellow' ), type=float)
-        self.list_all_categories()
-        category_id = click.prompt(click.style('Category ID ?', fg='yellow'), type=int)
+        if not product_price:
+            product_price = click.prompt(click.style('Price ?', fg='yellow' ), type=float)
 
-        confirm = click.confirm(click.style('Are you sure want to add this product?', fg='yellow', blink=True), default=True)
-        cursor = self.connection.cursor()
-        cursor.execute(add_product_query(product_name, product_price, category_id))
-        self.connection.commit()
-        click.secho(f"Added {product_name}", fg='blue')
-        self.list_all_products(category_id)
+        if not category_id:
+            self.list_all_categories()
+            category_id = click.prompt(click.style('Category ID ?', fg='yellow'), type=int)
+
+        if ask_confirm:
+            confirm = click.confirm(click.style('Are you sure want to add this product?', fg='yellow', blink=True), default=True)
+
+        if confirm:
+            self.execute_query(add_product_query(product_name, product_price, category_id))
+            self.connection.commit()
+            click.secho(f"Added {product_name}", fg='blue')
+            self.list_all_products(category_id)
+        else:
+            return self.admin_flow()
 
 
 class Cart:
 
-    def add_to_cart(self, product_id):
-        prompt_suffix = "(default is" + click.style(" 1", fg='magenta') + ')\t'
-        quantity = click.prompt("Quantity???", type=int, default=1,
-                        show_default=False, prompt_suffix=prompt_suffix)
-        cursor = self.connection.cursor()
-        cursor.execute(get_cart_id_query(self.user_id))
-        result = cursor.fetchone()
+    def add_to_cart(self, product_id, new_quantity=None):
+        if not new_quantity:
+            prompt_suffix = "(default is" + click.style(" 1", fg='magenta') + ')\t'
+            new_quantity = click.prompt("Quantity???", type=int, default=1,
+                            show_default=False, prompt_suffix=prompt_suffix)
 
+        cursor = self.execute_query(get_cart_id_query(self.user_id), get_dictionary=True)
+        result = cursor.fetchone()
         if result:
-            cart_id = result[0]
+            cart_id = result['cart_id']
         else:
             #create a cart for the user if not exist
-            cursor.execute(create_cart_query(self.user_id))
+            cursor = self.execute_query(create_cart_query(self.user_id))
             self.connection.commit()
             cart_id = cursor.lastrowid
 
-        cursor.execute(get_cart_prod_details_query(cart_id, product_id))
+        cursor = self.execute_query(get_cart_prod_details_query(cart_id, product_id), get_dictionary=True)
         result = cursor.fetchone()
-
         if result:
             # already have a cart add the product to it
-            cart_prod_id = result[0]
-            available_quantity = result[1]
-            cursor.execute(update_cart_product_query(
-                            cart_prod_id, available_quantity + quantity)
-                        )
+            cart_prod_id = result['cart_prod_id']
+            available_quantity = result['quantity']
+            self.execute_query(update_cart_product_query(cart_prod_id, available_quantity + new_quantity))
             self.connection.commit()
         else:
-            cursor.execute(create_cart_product_query(cart_id, product_id, quantity))
+            self.execute_query(create_cart_product_query(cart_id, product_id, new_quantity))
             self.connection.commit()
+        click.secho('Product successfully added to cart', fg='green')
 
-    def view_cart(self):
-        cursor = self.connection.cursor()
-        if self.is_admin:
-            self.get_all_user()
-            user_id = click.prompt(click.style('Enter user_id to show its cart...', fg='yellow'), type=int)
-            cursor.execute(view_cart_query(user_id))
-        else:
-            cursor.execute(view_cart_query(self.user_id))
+    def view_cart(self, user_id=None, ask_confirm=True):
+        if not user_id:
+            user_id = self.user_id
+            if self.is_admin:
+                self.get_all_user()
+                user_id = click.prompt(click.style('Enter user_id to show its cart...', fg='yellow'), type=int)
 
+        cursor = self.execute_query(view_cart_query(user_id))
         mytable = from_db_cursor(cursor)
         # mytable.hrules = 1
-        mytable.title = click.style('CART', fg='yellow', bold=True)
-        total_amount = self.get_total_amount_of_cart()
-        discounted_amount = self.get_discounted_amount(total_amount)
-        net_amount = total_amount - discounted_amount
+        if cursor.rowcount:
+            mytable.title = click.style('CART', fg='yellow', bold=True)
+            total_amount = self.get_total_amount_of_cart(user_id)
+            discounted_amount = self.get_discounted_amount(total_amount)
+            net_amount = total_amount - discounted_amount
 
-        total_row = ['']*(len(mytable.field_names)-2)
-        total_text = click.style('TOTAL', fg='green')
-        total_amount = click.style(str(total_amount), fg='green')
-        total_row.extend([total_text, total_amount ])
-        mytable.add_row(total_row)
+            total_row = ['']*(len(mytable.field_names)-2)
+            total_text = click.style('TOTAL', fg='green')
+            total_amount = click.style(str(total_amount), fg='green')
+            total_row.extend([total_text, total_amount ])
+            mytable.add_row(total_row)
 
-        if discounted_amount:
-            discount_row = ['']*(len(mytable.field_names)-2)
-            discount_text = click.style('DISCOUNT', fg='magenta')
-            discounted_amount = click.style(str(-discounted_amount), fg='magenta')
-            discount_row.extend([discount_text, discounted_amount ])
-            mytable.add_row(discount_row)
+            if discounted_amount:
+                discount_row = ['']*(len(mytable.field_names)-2)
+                discount_text = click.style('DISCOUNT', fg='magenta')
+                discounted_amount = click.style(str(-discounted_amount), fg='magenta')
+                discount_row.extend([discount_text, discounted_amount ])
+                mytable.add_row(discount_row)
 
-        net_row = ['']*(len(mytable.field_names)-2)
-        net_text = click.style('NET TO PAY', fg='green')
-        net_amount = click.style(str(net_amount), fg='green')
-        net_row.extend([net_text, net_amount ])
-        mytable.add_row(net_row)
-        print(mytable)
+            net_row = ['']*(len(mytable.field_names)-2)
+            net_text = click.style('NET TO PAY', fg='green')
+            net_amount = click.style(str(net_amount), fg='green')
+            net_row.extend([net_text, net_amount ])
+            mytable.add_row(net_row)
+            print(mytable)
+        else:
+            click.secho('Your Cart is empty.', fg='magenta', reverse=True)
 
     def remove_from_cart(self, **kwargs):
         cart_id = kwargs.get('cart_id')
@@ -318,13 +320,12 @@ class Cart:
             cart_id = click.prompt("Enter the cart_id", type=int)
             product_id = click.prompt("Enter the product_id", type=int)
 
-        cursor = self.connection.cursor()
-        cursor.execute(delete_from_cart_query(cart_id, product_id))
+        cursor = self.execute_query(delete_from_cart_query(cart_id, product_id))
         self.connection.commit()
 
-    def get_total_amount_of_cart(self):
+    def get_total_amount_of_cart(self, user_id):
         cursor = self.connection.cursor()
-        cursor.execute(get_total_amount_of_cart_query(self.user_id))
+        cursor.execute(get_total_amount_of_cart_query(user_id))
         final_amount = cursor.fetchone()
 
         if not final_amount or not final_amount[0]:
@@ -341,48 +342,46 @@ class Cart:
 
         return 0
 
-    def buy_from_cart(self):
-        total_amount = self.get_total_amount_of_cart()
+    def buy_from_cart(self, ask_confirm=True, confirm=True):
+        total_amount = self.get_total_amount_of_cart(self.user_id)
         discounted_amount = self.get_discounted_amount(total_amount)
 
-        confirm = click.confirm('Are you sure want to buy this ?', default=True)
-        if not confirm:
-            return self.customer_flow()
-        else:
-            cursor = self.connection.cursor()
-            cursor.execute(buy_from_cart_query(self.user_id, total_amount, discounted_amount))
-            self.connection.commit()
+        if ask_confirm:
+            confirm = click.confirm('Are you sure want to buy this ?', default=True)
+            if not confirm:
+                return self.customer_flow()
 
-            cursor = self.connection.cursor()
-            cursor.execute(get_last_insert_id())
-            order_id = cursor.fetchone()[0]
+        self.execute_query(buy_from_cart_query(self.user_id, total_amount, discounted_amount))
+        self.connection.commit()
 
-            cursor = self.connection.cursor()
-            cursor.execute(get_order_details_query(self.user_id))
-            all_rows = cursor.fetchall()
-            data_to_be_inserted = list()
+        cursor = self.execute_query(get_last_insert_id())
+        order_id = cursor.fetchone()[0]
 
-            cart_id = -1
-            if len(all_rows):
-                cart_id = all_rows[0][0]
+        cursor = self.execute_query(get_order_details_query(self.user_id))
+        all_rows = cursor.fetchall()
+        data_to_be_inserted = list()
 
-            for row in all_rows:
-                row = list(row)
-                row[0] = order_id
-                row = tuple(row)
-                data_to_be_inserted.append(row)
+        cart_id = -1
+        if len(all_rows):
+            cart_id = all_rows[0][0]
 
-            #insert the product details
-            cursor = self.connection.cursor()
-            cursor.executemany(create_into_order_details_query(), data_to_be_inserted)
-            self.connection.commit()
+        for row in all_rows:
+            row = list(row)
+            row[0] = order_id
+            row = tuple(row)
+            data_to_be_inserted.append(row)
 
-            cursor = self.connection.cursor()
-            cursor.execute(delete_from_cart(cart_id))
-            self.connection.commit()
+        #insert the product details
+        cursor = self.connection.cursor()
+        cursor.executemany(create_into_order_details_query(), data_to_be_inserted)
+        self.connection.commit()
 
-            print("\nThank you for shopping at MyCart.")
-            print("Have a Good Day.")
+        #emptying cart after buying the products
+        cursor = self.execute_query(delete_from_cart(cart_id))
+        self.connection.commit()
+
+        print("\nThank you for shopping at MyCart.")
+        print("Have a Good Day.")
 
 
 class MyCart(DB_set_up, User, Product, Cart):
