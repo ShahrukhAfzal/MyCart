41736af8ae31e0eaa71b25e9ccf61b4db1b4654a
shahrukh <shoaibtayyab121@gmail.com>
2020-12-09 15:52:44 +0530

    creating fixtures for testing

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -1,17 +1,19 @@
-admin_user = {
-    'id' : 1,
-    'user_name' : 'admin',
-    'user_email' : 'admin@gmail.com',
-    'is_admin': 1,
-    'password':'123456'
+users_fixtures = {
+    'admin_user': [{
+        'id' : 1,
+        'user_name' : 'admin',
+        'user_email' : 'admin@gmail.com',
+        'is_admin': 1,
+        'password':'123456'
 
-}
+    }],
+    'customer_user': [{
+        'id' : 2,
+        'user_name' : 'shahrukh',
+        'user_email' : 'shoaibtayyab121@gmail.com',
+        'is_admin': 0,
+        'password':'123456'
 
-customer_user = {
-    'id' : 2,
-    'user_name' : 'shahrukh',
-    'user_email' : 'shoaibtayyab121@gmail.com',
-    'is_admin': 0,
-    'password':'123456'
+    }]
 
 }
